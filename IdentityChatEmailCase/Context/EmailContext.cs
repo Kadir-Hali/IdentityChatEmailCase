b75e9fa24986e465a6d18c9136f85a56e6add10c
Kadir-Hali <kadirhali07@gmail.com>
2025-05-09 16:02:46 +0300

    Add project files.

---
+++ IdentityChatEmailCase/Context/EmailContext.cs
@@ -0,0 +1,16 @@
+﻿using IdentityChatEmailCase.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityChatEmailCase.Context
+{
+    public class EmailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS; Initial Catalog=EmailChatCaseDb; Integrated Security=true; Trust Server Certificate=true");
+        }
+
+        
+    }
+}
---
+++ IdentityChatEmailCase/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IdentityChatEmailCase.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? City { get; set; }
+        public string? ProfileImageUrl { get; set; }
+
+    }
+}
